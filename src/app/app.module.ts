fc21463db756634d8d67ce428a9f084a8f405363
Jaedan Seabaugh <113535466+janse7155@users.noreply.github.com>
2023-11-10 14:06:40 -0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { FirstFiveOrdersComponent } from './first-five-orders/first-five-orders.component';
+import { AllOtherOrdersComponent } from './all-other-orders/all-other-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderDashboardComponent,
+    FirstFiveOrdersComponent,
+    AllOtherOrdersComponent
   ],
   imports: [
     BrowserModule,
